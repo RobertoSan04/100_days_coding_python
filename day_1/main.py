8fa27895052843cc95d1830e08befc9343ba6099
Roberto Sánchez Santoyo <robertosanchezsantoyo@outlook.com>
2025-10-29 14:41:08 -0600

    Day 1

---
+++ day_1/main.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    print("Hello, my name is Roberto")
+    name = input("What's your name? ")
+    age = input("How old are you? ")
+    print(f"Hello {name} it's nice to know another {age} years old")
