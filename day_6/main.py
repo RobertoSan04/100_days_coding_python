5b888433f160f0ee70826e7499dfab550144ea4f
Roberto Sanchez Santoyo <robertosanchezsantoyo@gmail.com>
2026-08-31 10:57:11 -0600

    Start of day 15

---
+++ day_6/main.py
@@ -0,0 +1,15 @@
+def my_function():
+    print("Hello")
+    print("Bye")
+
+def main() -> None:
+
+    # 44. Defining and Calling Python Functions
+    my_function()
+
+    # 45. The Hurdles Loop Challenge
+
+
+
+if __name__ == "__main__":
+    main()
